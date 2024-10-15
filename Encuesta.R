4d4ea52976ff42a65ab00ecbd0ef45236f14ee7b
David <davidroberto.tapia@edu.uah.es>
2024-10-15 19:33:58 +0200

    Intervalos principio

--- Encuesta.R
+++ Encuesta.R
@@ -5,29 +5,73 @@ print(encuesta)
 edad = encuesta$Edad;
 print(edad)
 
+ #División entre sexos
 sexo = encuesta$Sexo;
 print(sexo)
 
+#Número de personas con cada edad
+hombres = encuesta$Total[encuesta$Sexo == "Hombre"]
+mujeres = encuesta$Total[encuesta$Sexo == "Mujer"]
+print(hombres)
+print(mujeres)
+
+#Total de inmigraciones por sexos
 total = encuesta$Total
 print(total)
 
-mayoresEdad = encuesta$Total[encuesta$Edad>=18];
-print(mayoresEdad)
-print(length(mayoresEdad))
+ #Hombres
+hombresInmgr = sum(hombres)
+print(hombresInmgr)
 
-menoresEdad = encuesta$Total[encuesta$Edad<18];
-print(menoresEdad)
-print(length(menoresEdad))
+ #Mujeres
+mujeresInmgr = sum(mujeres)
+print(mujeresInmgr)
 
-hombres = encuesta$Total[encuesta$Sexo == "Hombre"]
-mujeres = encuesta$Total[encuesta$Sexo == "Mujer"]
+#Mayores y menores de edad en hombres y mujeres y el total
+ #Hombres
+     #Mayores de edad
+mayoresEdadHombres = encuesta$Total[encuesta$Edad>=18 & encuesta$Sexo == "Hombre"];
+print(mayoresEdadHombres)
+print(length(mayoresEdadHombres))
 
-#medias de centralizacion
+totalMayoresEdadHombres = sum(mayoresEdadHombres)
+print(totalMayoresEdadHombres) 
+
+     #Menores de edad
+menoresEdadHombres = encuesta$Total[encuesta$Edad<18 & encuesta$Sexo == "Hombre"];
+print(menoresEdadHombres)
+print(length(menoresEdadHombres))
+
+totalMenoresEdadHombres = sum(menoresEdadHombres)
+print(totalMenoresEdadHombres)
+
+  #Mujeres
+      #Mayores de edad
+mayoresEdadMujeres = encuesta$Total[encuesta$Edad>=18 & encuesta$Sexo == "Mujer"];
+print(mayoresEdadMujeres)
+print(length(mayoresEdadMujeres))
+
+totalMayoresEdadMujeres = sum(mayoresEdadMujeres)
+print(totalMayoresEdadMujeres)
+
+       #Menores de edad
+menoresEdadMujeres = encuesta$Total[encuesta$Edad<18 & encuesta$Sexo == "Mujer"];
+print(menoresEdadMujeres)
+print(length(menoresEdadMujeres))
+
+totalMenoresEdadMujeres = sum(menoresEdadMujeres)
+print(totalMenoresEdadMujeres)
+
+totalMujeres = totalMayoresEdadMujeres + totalMenoresEdadMujeres
+print(totalMujeres)
+
+# Medias de centralización
   #Media
   media_hombres_por_edad = mean(hombres)
   print(media_hombres_por_edad)
   media_mujeres_por_edad = mean(mujeres)
   print(media_mujeres_por_edad)
+  
   #Mediana
   mediana_hombres_por_edad = median(hombres)
   print(mediana_hombres_por_edad)
@@ -38,12 +82,16 @@ mujeres = encuesta$Total[encuesta$Sexo == "Mujer"]
   #Rango
   rango_de_edad = range(edad)
   max(edad)-min(edad)
+  print(rango_de_edad)
+  
   #Varianza (muestral) 
   var(hombres) 
   var(mujeres)
+  
   #Desviación típica 
   sd(hombres) 
   sd(mujeres)
+  
   #Coeficiente de variación 
   sd(hombres)/mean(hombres) 
   sd(mujeres)/mean(mujeres) 
@@ -52,6 +100,7 @@ mujeres = encuesta$Total[encuesta$Sexo == "Mujer"]
   #Cuartiles
   summary(hombres)
   summary(mujeres)
+  
   #Rango intercuartílico
   IQR(hombres)
   IQR(mujeres)
@@ -65,3 +114,17 @@ mujeres = encuesta$Total[encuesta$Sexo == "Mujer"]
   #Coeficiente de apuntamiento o curtosis
   kurtosis(hombres)
   kurtosis(mujeres)
+
+#Distribuciones de frecuencia
+  #Intervalos nº de personas inmigrantes por sexo(50, 5000, 10000, 15000, 20000)
+  
+  #Hombres
+inmigracionesHombres = cut(hombres, breaks=c(50, 5000, 10000, 15000, 20000), right=FALSE, include.lowest=TRUE)
+table(inmigracionesHombres)
+cumsum(table(inmigracionesHombres))
+
+  #Mujeres
+inmigracionesMujeres = cut(mujeres, breaks=c(50, 5000, 10000, 15000, 20000), right=FALSE, include.lowest=TRUE)
+table(inmigracionesMujeres)
+cumsum(table(inmigracionesMujeres))
+
