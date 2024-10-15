4da7731f8fada340a9bce85a20ce28a71de2546a
nizzma <nicolas.ibanez@edu.uah.es>
2024-10-15 13:33:08 +0200

    Update Encuesta.R

--- Encuesta.R
+++ Encuesta.R
@@ -23,42 +23,45 @@ hombres = encuesta$Total[encuesta$Sexo == "Hombre"]
 mujeres = encuesta$Total[encuesta$Sexo == "Mujer"]
 
 #medias de centralizacion
-media_hombres_por_edad = mean(hombres)
-print(media_hombres_por_edad)
-media_mujeres_por_edad = mean(mujeres)
-print(media_mujeres_por_edad)
-
-mediana_hombres_por_edad = median(hombres)
-print(mediana_hombres_por_edad)
-mediana_mujeres_por_edad = median(mujeres)
-print(mediana_mujeres_por_edad)
+  #Media
+  media_hombres_por_edad = mean(hombres)
+  print(media_hombres_por_edad)
+  media_mujeres_por_edad = mean(mujeres)
+  print(media_mujeres_por_edad)
+  #Mediana
+  mediana_hombres_por_edad = median(hombres)
+  print(mediana_hombres_por_edad)
+  mediana_mujeres_por_edad = median(mujeres)
+  print(mediana_mujeres_por_edad)
 
 #medias de dispersion
-rango_de_edad = range(edad)
-max(edad)-min(edad)
-
-var(hombres) 
-var(mujeres)
-
-sd(hombres) 
-sd(mujeres)
-
-sd(hombres)/mean(hombres) 
-sd(mujeres)/mean(mujeres) 
+  #Rango
+  rango_de_edad = range(edad)
+  max(edad)-min(edad)
+  #Varianza (muestral) 
+  var(hombres) 
+  var(mujeres)
+  #Desviación típica 
+  sd(hombres) 
+  sd(mujeres)
+  #Coeficiente de variación 
+  sd(hombres)/mean(hombres) 
+  sd(mujeres)/mean(mujeres) 
 
 #Medidas de localización
-summary(hombres)
-summary(mujeres)
-
-IQR(hombres)
-IQR(mujeres)
+  #Cuartiles
+  summary(hombres)
+  summary(mujeres)
+  #Rango intercuartílico
+  IQR(hombres)
+  IQR(mujeres)
 
 #Medidas de forma
-install.packages("e1071")
-library(e1071)
-
-skewness(hombres)
-skewness(mujeres)
-
-kurtosis(hombres)
-kurtosis(mujeres)
+  install.packages("e1071")
+  library(e1071)
+  #Coeficiente de asimetría 
+  skewness(hombres)
+  skewness(mujeres)
+  #Coeficiente de apuntamiento o curtosis
+  kurtosis(hombres)
+  kurtosis(mujeres)
